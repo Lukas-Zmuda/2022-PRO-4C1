edbdd36e93aa8b9e51c71894bce7111f8573f862
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-10-10 09:44:22 +0200

    Update PrvyProjekt.java

--- MojPrvyProjekt/src/PrvyProjekt.java
+++ MojPrvyProjekt/src/PrvyProjekt.java
@@ -2,8 +2,35 @@
 public class PrvyProjekt {
 
 	public static void main(String[] args) {
+
+		//jednoriadkovy komentar
 		
+		/*
+		 * toto je 
+		 * viacriadkovy 
+		 * komentar
+		 */
+		
+		/**
+		 * javadoc
+		 * komentar
+		 */
 		System.out.println("Ahoj 4C");
+		
+		System.out.println(10 + 5);
+		
+		System.out.println(10 / 3);
+		
+		System.out.println((10 + 5) * 2);
+		
+		System.out.println("Jan" + "Hrasko");
+		
+		System.out.println("Jan" + 2);
+		
+		System.out.println("10" + 5);
+		
+		System.out.println("10 + 5 = " + (10 + 5));
+		
 
 	}
 
